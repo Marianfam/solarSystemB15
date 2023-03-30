16e78ee1441c235cd500b5635ba6f08b693d0242
marian fam <m.golgos@gmail.com>
2023-03-30 15:07:46 -0400

    i added the feature sun there are errires that to be resolved

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         SolarSystem Solar=new SolarSystem();
-        String sun="1";
-        String moon="2";
-        String planets="2";
+    Solar.sun="1";
+        Solar. moon="2";
+       Solar. planets="2";
+
+       featureSun Xron = new featureSun();
+       Xron . temperature = "12312313k";
+       Xron . radius = 1312313133;
+       Xron . color = "Pink";
+
     }
 }
