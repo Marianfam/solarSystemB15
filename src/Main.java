5297cccb4e32947935cb144d6812a3d22943800c
marian fam <m.golgos@gmail.com>
2023-03-30 11:40:33 -0400

    The basic operationl model of the solar system

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem Solar=new SolarSystem();
+        String sun="1";
+        String moon="2";
+        String planets="2";
+    }
+}
---
+++ src/SolarSystem.java
@@ -0,0 +1,11 @@
+public class SolarSystem {
+    String sun;
+    String moon;
+    String planets;
+    //Develop a solar System basic operational model and deploy
+    //1-write the code
+    //2- test it and commit it in the local repo
+    //3- push the coda to github
+
+
+}
